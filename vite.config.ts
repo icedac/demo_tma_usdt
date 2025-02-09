a57af66703f87be895d6d376a53993b044479cb1
GShock <icedac@gmail.com>
2025-02-10 01:38:31 +0900

    x

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/demo_tma_usdt/',
   plugins: [react(), tsconfigPaths()],
   publicDir: './public',
 });
